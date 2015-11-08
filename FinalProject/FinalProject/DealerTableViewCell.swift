7cb4a6895c2d63afac116f5459f58cf6d63e20b5
johntr <jtredlich@gmail.com>
2015-11-08 00:47:09 -0500

    Final Project, Inital commit. TableView in Progress

---
+++ FinalProject/FinalProject/DealerTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DealerTableViewCell.swift
+//  
+//
+//  Created by John Redlich on 11/7/15.
+//
+//
+
+import UIKit
+
+class DealerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
